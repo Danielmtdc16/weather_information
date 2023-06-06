c4b0628f8dfac363453f4181afc3b8df2839776c
Daniel <danielmtdc16@gmail.com>
2023-06-06 19:06:05 -0300

    requisição funcionando.

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+let city_name_input = 'london';
+
+function for_capital_letter(city_name) {
+    const list_of_words = city_name.split(" ");
+    return list_of_words.map((word) => {
+        return word[0].toUpperCase() + word.substring(1);
+    }).join("");
+}
+
+city_name_input = for_capital_letter(city_name_input);
 
 let api_key = "1133ef7759c0bd7f313fd8ebc768a559";
 
+const api_geographic_coordinates = `http://api.openweathermap.org/geo/1.0/direct?q=${city_name_input}&limit=1&appid=${api_key}`;
+
+console.log(api_geographic_coordinates);
+
 const api_url = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${api_key}`;
 
-function request(url) {
-    fetch(url).then((data) => data.json()).then((resp) => console.log(resp['coord']['lon']));
+function coordinate_request(url) {
+    fetch(url).then((data) => {
+        return data.json();
+    }).then((resp) => {
+        console.log(resp[0]["lat"]);
+    });
 }
 
-request(api_url);
+console.log(coordinate_request(api_geographic_coordinates));
